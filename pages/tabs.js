a2742ed8f022e455f556e24cb5539e23115ca974
mallium <mallium@gmail.com>
2023-04-26 18:37:17 -0400

    ejemplo con tabs v2

--- pages/tabs.js
+++ pages/tabs.js
@@ -3,13 +3,13 @@ import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 
 const EjemploTabs = () => {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Ejemplo de Tabs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <div>
         <Tabs>
           <TabList>
             <Tab>One</Tab>
@@ -19,7 +19,12 @@ const EjemploTabs = () => {
 
           <TabPanels>
             <TabPanel>
-              <p>one!</p>
+              <div>
+                <p>Hola!!</p>
+              </div>
+              <div style={{ backgroundColor: "lightblue", padding: "10px" }}>
+                <p>hfah ahf akgj kja faskf jfsa</p>
+              </div>
             </TabPanel>
             <TabPanel>
               <p>two!</p>
@@ -29,7 +34,7 @@ const EjemploTabs = () => {
             </TabPanel>
           </TabPanels>
         </Tabs>
-      </main>
+      </div>
     </div>
   );
 };
